141a7d65c7f1bc83d4a4fbbc7f04e852c40c99b1
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-16 08:55:08 -0600

    added SQL statements and removed extra POC folder

---
+++ setupDatabase.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR (50) UNIQUE,
+    last_name VARCHAR (50) UNIQUE
+);
+
+CREATE TABLE tabletops (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR (50) UNIQUE,
+    capacity INTEGER,
+    status VARCHAR (50),
+    employee_id INTEGER REFERENCES employees(id) ON DELETE SET NULL
+);
+
+SELECT * from tabletops
+LEFT JOIN employees
+ON employees.id = tabletops.employee_id;
